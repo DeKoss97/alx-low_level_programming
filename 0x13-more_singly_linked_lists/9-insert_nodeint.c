5ac5c332de2619e29a17672937598678263c1751
DeKoss97 <109168125+DeKoss97@users.noreply.github.com>
2022-10-25 07:57:32 +0200

    9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,121 @@
-#include "lists.h"
+include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: the linked list
+ *
+ * Return: returns the number of elements in a linked list
+ */
+
+size_t list_len(const listint_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
+ * new_nodeint - Creates a new node for list
+ * @n: number to add to new node
  *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
+ * Return: returns the new node or NULL on failure
+ */
+listint_t *new_nodeint(const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	return (new);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to the head of the list
+ * @n: number to add to new node
  *
- * Return: NULL if anything fails or the address of the new node
+ * Return: the address of the new element, or NULL on failure
+ */
+listint_t *add_node(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = new_nodeint(n);
+	if (new == NULL)
+		return (new);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
+/**
+* add_node_end - adds a new node at the end of a linked list
+* @head: pointer to the head of the linked list
+* @n: number to add to new node
+*
+* Return: returns the address of the new element, or NULL on failure
 */
+listint_t *add_node_end(listint_t **head, const int n)
+{
+	listint_t *new, *last;
+
+	last = *head;
+	if (head == NULL)
+		return (NULL);
+	new = new_nodeint(n);
+	if (new == NULL)
+		return (NULL);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	return (new);
+}
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: the linked list
+ * @idx: the index where we are inserting
+ * @n: number for new node
+ *
+ * Return: retunrs the address of the new node, or NULL on failure
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int index;
+	unsigned int x;
+	listint_t *new, *temp;
 
-	current = *head; /*place first node at current*/
-
-	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
+	if (head == NULL || idx > list_len(*head))
 		return (NULL);
-
-	new_node->n = n; /* add our element to the new node*/
-
-	/*iterate list to node position idx - 2*/
-	for (index = 0; head != NULL && index < idx - 1; index++)
+	else if (idx == 0)
+		return (add_node(head, n));
+	else if (idx == list_len(*head))
+		return (add_node_end(head, n));
+	x = 0;
+	new = new_nodeint(n);
+	temp = *head;
+	if (new == NULL)
+		return (NULL);
+	while (x < idx - 1)
 	{
-		current = current->next;
-		if (current == NULL)
-			return (NULL);
+		temp = temp->next;
+		x++;
 	}
-
-	if (idx == 0) /*if the index for new node is 0*/
-	{
-		/*first node will be moved to second node*/
-		new_node->next = *head;
-		/*new node will be placed as the first node*/
-		*head = new_node;
-	}
-	else if (current->next) /*if index where to add our new node is not 0*/
-	{
-		new_node->next = current->next; /*place current node after new node*/
-		current->next = new_node;/*set the new node at index idx*/
-	}
-	else /*if node position is not present in the list*/
-	{
-		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
-		current->next = new_node;/*set the new node at the last position in list*/
-	}
-
-	return (new_node);
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
